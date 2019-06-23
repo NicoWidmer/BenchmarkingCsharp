efc29ce3b48ca3041ef6b1667097bad9ec2420bf
Nico Widmer <willyfisch@yahoo.com>
2019-06-23 14:37:23 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading;
+
+namespace BenchmarkingCsharp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int durationMilliseconds = 2000;
+
+            Workloads workloads = new Workloads();
+            Thread thread;
+
+
+            thread = new Thread(new ThreadStart(workloads.StartIntegerAdd));
+            Console.WriteLine("Benchmarking int add operations...");
+            Thread.Sleep(200);
+            thread.Start();
+            Thread.Sleep(durationMilliseconds);
+            thread.Abort();
+            Console.WriteLine("Int add operations per ms: " + workloads.GetExecutedOperations() / durationMilliseconds);
+
+            
+            thread = new Thread(new ThreadStart(workloads.StartIntegerMultiply));
+            Console.WriteLine();
+            Console.WriteLine("Benchmarking int multiply operations...");
+            Thread.Sleep(200);
+            thread.Start();
+            Thread.Sleep(durationMilliseconds);
+            thread.Abort();
+            Console.WriteLine("Int multiply operations per ms: " + workloads.GetExecutedOperations() / durationMilliseconds);
+
+
+            thread = new Thread(new ThreadStart(workloads.StartIntegerDivide));
+            Console.WriteLine();
+            Console.WriteLine("Benchmarking int divide operations...");
+            Thread.Sleep(200);
+            thread.Start();
+            Thread.Sleep(durationMilliseconds);
+            thread.Abort();
+            Console.WriteLine("Int divide operations per ms: " + workloads.GetExecutedOperations() / durationMilliseconds);
+
+
+            thread = new Thread(new ThreadStart(workloads.StartBranchyIntegerAdd));
+            Console.WriteLine();
+            Console.WriteLine("Benchmarking int branchy add operations...");
+            Thread.Sleep(200);
+            thread.Start();
+            Thread.Sleep(durationMilliseconds);
+            thread.Abort();
+            Console.WriteLine("Int branchy operations per ms: " + workloads.GetExecutedOperations() / durationMilliseconds);
+
+
+            thread = new Thread(new ThreadStart(workloads.StartFloatAdd));
+            Console.WriteLine();
+            Console.WriteLine("Benchmarking float add operations...");
+            Thread.Sleep(200);
+            thread.Start();
+            Thread.Sleep(durationMilliseconds);
+            thread.Abort();
+            Console.WriteLine("Float add operations per ms: " + workloads.GetExecutedOperations() / durationMilliseconds);
+
+
+            thread = new Thread(new ThreadStart(workloads.StartFloatMultiply));
+            Console.WriteLine();
+            Console.WriteLine("Benchmarking float multiply operations...");
+            Thread.Sleep(200);
+            thread.Start();
+            Thread.Sleep(durationMilliseconds);
+            thread.Abort();
+            Console.WriteLine("Float multiply operations per ms: " + workloads.GetExecutedOperations() / durationMilliseconds);
+
+
+            thread = new Thread(new ThreadStart(workloads.StartFloatDivide));
+            Console.WriteLine();
+            Console.WriteLine("Benchmarking float divide operations...");
+            Thread.Sleep(200);
+            thread.Start();
+            Thread.Sleep(durationMilliseconds);
+            thread.Abort();
+            Console.WriteLine("Float divide operations per ms: " + workloads.GetExecutedOperations() / durationMilliseconds);
+
+
+            thread = new Thread(new ThreadStart(workloads.StartVectorMultiply));
+            Console.WriteLine();
+            Console.WriteLine("Benchmarking vector multiply operations...");
+            Thread.Sleep(200);
+            thread.Start();
+            Thread.Sleep(durationMilliseconds);
+            thread.Abort();
+            Console.WriteLine("Multiply per ms using vectors: " + workloads.GetExecutedOperations() / durationMilliseconds);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Workloads.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Numerics;
+
+namespace BenchmarkingCsharp
+{
+    class Workloads
+    {
+
+        private const int ArraySize = 1024;
+        private const int ArraySizeVector = ArraySize / 4;
+        private readonly int[] intSourceArray = new int[ArraySize];
+        private int[] intResultArray = new int[ArraySize];
+        private readonly float[] floatSourceArray = new float[ArraySize];
+        private float[] floatResultArray = new float[ArraySize];
+        private readonly Vector4[] vectorSourceArray = new Vector4[ArraySizeVector];
+        private Vector4[] vectorResultArray = new Vector4[ArraySizeVector];
+
+        private long loopCount;
+
+        public Workloads()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < ArraySize; i++)
+            {
+                intSourceArray[i] = rnd.Next(int.MinValue / 2 + 1, int.MaxValue / 2 - 1);
+                floatSourceArray[i] = (float)rnd.NextDouble();
+            }
+
+            for (int i = 0; i < ArraySizeVector; i++)
+            {
+                vectorSourceArray[i].W = (float)rnd.NextDouble();
+                vectorSourceArray[i].X = (float)rnd.NextDouble();
+                vectorSourceArray[i].Y = (float)rnd.NextDouble();
+                vectorSourceArray[i].Z = (float)rnd.NextDouble();
+            }
+        }
+
+        public void StartIntegerAdd()
+        {
+            loopCount = 0;
+
+            while (true)
+            {
+                IntegerAdd();
+                loopCount++;
+            }
+        }
+
+        public void StartIntegerMultiply()
+        {
+            loopCount = 0;
+
+            while (true)
+            {
+                IntegerMultiply();
+                loopCount++;
+            }
+        }
+
+        public void StartIntegerDivide()
+        {
+            loopCount = 0;
+
+            while (true)
+            {
+                IntegerDivide();
+                loopCount++;
+            }
+        }
+
+        public void StartBranchyIntegerAdd()
+        {
+            loopCount = 0;
+
+            while (true)
+            {
+                BranchyIntegerAdd();
+                loopCount++;
+            }
+        }
+
+        public void StartFloatAdd()
+        {
+            loopCount = 0;
+
+            while (true)
+            {
+                FloatAdd();
+                loopCount++;
+            }
+        }
+
+        public void StartFloatMultiply()
+        {
+            loopCount = 0;
+
+            while (true)
+            {
+                FloatMultiply();
+                loopCount++;
+            }
+        }
+
+        public void StartFloatDivide()
+        {
+            loopCount = 0;
+
+            while (true)
+            {
+                FloatDivide();
+                loopCount++;
+            }
+        }
+
+        public void StartVectorMultiply()
+        {
+            loopCount = 0;
+
+            while (true)
+            {
+                VectorMultiply();
+                loopCount++;
+            }
+        }
+
+        public long GetExecutedOperations()
+        {
+            return loopCount * ArraySize;
+        }
+
+
+        private void IntegerAdd()
+        {
+            for (int i = 0; i < ArraySize; i++)
+            {
+                intResultArray[i] = intSourceArray[i] + 1;
+            }
+        }
+
+        private void IntegerMultiply()
+        {
+            for (int i = 0; i < ArraySize; i++)
+            {
+                intResultArray[i] = intSourceArray[i] * 2;
+            }
+        }
+
+        private void IntegerDivide()
+        {
+            for (int i = 0; i < ArraySize; i++)
+            {
+                intResultArray[i] = intSourceArray[i] / 2;
+            }
+        }
+
+        private void BranchyIntegerAdd()
+        {
+            for (int i = 0; i < ArraySize; i++)
+            {
+                if (intResultArray[i] > 0)
+                {
+                    intResultArray[i] = intSourceArray[i] - 1;
+                }
+                else
+                {
+                    intResultArray[i] = intSourceArray[i] + 1;
+                }
+            }
+        }
+
+        private void FloatAdd()
+        {
+            for (int i = 0; i < ArraySize; i++)
+            {
+                floatResultArray[i] = floatSourceArray[i] + 1.0f;
+            }
+        }
+
+        private void FloatMultiply()
+        {
+            for (int i = 0; i < ArraySize; i++)
+            {
+                floatResultArray[i] = floatSourceArray[i] * 0.5f;
+            }
+        }
+
+        private void FloatDivide()
+        {
+            for (int i = 0; i < ArraySize; i++)
+            {
+                floatResultArray[i] = floatSourceArray[i] / 2f;
+            }
+        }
+
+        private void VectorMultiply()
+        {
+            for (int i = 0; i < ArraySizeVector; i++)
+            {
+                vectorResultArray[i] = Vector4.Multiply(2.0f, vectorSourceArray[i]);
+            }
+        }
+
+    }
+}
